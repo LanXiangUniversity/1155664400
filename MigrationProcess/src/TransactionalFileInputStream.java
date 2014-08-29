4fe7d203bd1ef6690a3bab31998754081a63eb6b
parasitew <tongwei199311@gmail.com>
2014-08-28 23:37:44 -0400

    Framework of lab1

--- MigrationProcess/src/TransactionalFileInputStream.java
+++ MigrationProcess/src/TransactionalFileInputStream.java
@@ -1,5 +1,16 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+
 /**
  * Created by parasitew on 8/28/14.
  */
-public class TransactionalFileInputStream {
+public class TransactionalFileInputStream extends InputStream implements Serializable {
+	private FileStatus fileStatus;
+	private int position;
+
+	@Override
+	public int read() throws IOException {
+		return 0;
+	}
 }

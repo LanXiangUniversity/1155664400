5821f2379f1613977f0db3917bda0aaa1814c938
parasitew <tongwei199311@gmail.com>
2014-09-11 16:32:50 -0400

    Framework of lab1

---
+++ MigrationProcess/src/processes/SqrtProcess.java
@@ -0,0 +1,37 @@
+package processes;
+
+/**
+ * Created by Wei on 9/10/14.
+ */
+public class SqrtProcess implements MigratableProcess {
+	private volatile boolean suspending;
+	private double n = 0;
+	private double k = 1.0;
+
+	public SqrtProcess(String[] args) {
+		this.n = Double.parseDouble(args[0]);
+	}
+
+	@Override
+	public void run() {
+		while (!this.suspending && Math.abs(k*k-n) > 1e-9) {
+			k=(k+n/k)/2;
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("sqrt of " + n + " is " + k);
+
+		this.suspending = false;
+	}
+
+	@Override
+	public void suspend() {
+		this.suspending = true;
+		while(this.suspending) {};
+	}
+}
